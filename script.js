880fd6fc6591446666c4451c8f01160907a5bb93
André Carvalho Gomes <andrecarvalho388@gmail.com>
2025-10-14 21:38:07 -0300

    agora a idade é dinamica

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ setInterval(() => { getHora()}, "1000");
             let time = h + ":" + m ;
             tag.innerText=time
         }
+const paginaCarregada  = () => {
+  console.log("eu fui executado quando a pagina foi carregada")
+  let identidadeElement = document.getElementById("identidade");
+
+  // cria dois elementos datas, um com a data atual e outro com a data do nascimento e subitrai os dois, depois dividos por 1000m, 60s,60m,24h,365d
+  let idade = parseInt((new Date() - new Date(1993,2,26))/(1000*60*60*24*365));
+  let idadeElement = document.createElement("li")
+   idadeElement.innerText= `Idade: ${idade} anos`
+  identidadeElement.appendChild(idadeElement) }
